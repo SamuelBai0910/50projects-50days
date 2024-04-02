75d915123bd9bafa8f933d2a24562e3300da656d
SamuelBai0910 <baisimo2022@163.com>
2024-04-02 13:48:34 +1100

    update css

--- 04-Hidden-serach/script.js
+++ 04-Hidden-serach/script.js
@@ -1,9 +1,8 @@
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
 
-btn.addEventListener("click", () => update());
-
-function update() {
-  input.classList.toggle("close");
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
   input.focus();
-}
+});
