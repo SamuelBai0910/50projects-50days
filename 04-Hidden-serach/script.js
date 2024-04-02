bdc0cbb5ba7500518f1632d216e7cf0b6d34e8f9
SamuelBai0910 <baisimo2022@163.com>
2024-04-02 14:31:04 +1100

    update the opposite version

--- 04-Hidden-serach/script.js
+++ 04-Hidden-serach/script.js
@@ -3,6 +3,6 @@ const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
 
 btn.addEventListener("click", () => {
-  search.classList.toggle("active");
+  search.classList.toggle("close");
   input.focus();
 });
